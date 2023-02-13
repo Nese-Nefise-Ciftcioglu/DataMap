43907e41dc1c89b5057437aa8b6ea7914745aa05
neşe nefise çiftçioğlu <nesenefise@hotmail.com>
2023-02-13 10:34:10 +0300

    Add project files.

---
+++ DataMap/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace DataMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notificationLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Surname";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.addressBox);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.nameBox);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 175);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.surnameBox);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(136, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 38);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Update mapping";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.UpdateMapButton);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(136, 296);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 38);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.SaveButton);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(287, 46);
+            this.panel1.TabIndex = 8;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(248, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.CloseButton);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(214, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "_";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.MinimizeButton);
+            // 
+            // notificationLabel
+            // 
+            this.notificationLabel.AutoSize = true;
+            this.notificationLabel.Location = new System.Drawing.Point(53, 217);
+            this.notificationLabel.Name = "notificationLabel";
+            this.notificationLabel.Size = new System.Drawing.Size(0, 13);
+            this.notificationLabel.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 369);
+            this.ControlBox = false;
+            this.Controls.Add(this.notificationLabel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label notificationLabel;
+    }
+}
+
---
+++ DataMap/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Threading.Tasks;
+namespace DataMap
+{
+    public partial class Form1 : Form
+    {
+        public string location;
+        public string value;
+        public static SqlConnection conn = new SqlConnection(@"Data Source=" + Environment.MachineName + @"\SQLEXPRESS" + " ;Initial Catalog= test_db ;Integrated Security=SSPI;MultipleActiveResultSets=True");
+        public string addressLoc;
+        public string nameLoc;
+        public string surnameLoc;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Write the initial values from database to text boxes
+            conn.Open();
+            List<String> excelBlocks = new List<String>();
+            string query = "Select * from ExcelMap";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string output = reader.GetValue(1).ToString();
+                excelBlocks.Add(output.ToString()); ;
+            }
+            textBox1.Text = excelBlocks[0];
+            textBox2.Text = excelBlocks[1];
+            textBox3.Text = excelBlocks[2];
+            conn.Close();
+        }
+
+        //--- Set values for ExcelBlock
+        private void UpdateMapButton(object sender, EventArgs e)
+        {
+            //button3.BackColor = Color.DarkGray;
+            conn.Open();
+            SqlCommand cmdAdrs = new SqlCommand("UPDATE ExcelMap SET ExcelBlock = @adrs Where CloumnName = 'Address' ", conn);
+            SqlCommand cmdName = new SqlCommand("UPDATE ExcelMap SET ExcelBlock = @Name Where CloumnName = 'Name' ", conn);
+            SqlCommand cmdSurname = new SqlCommand("UPDATE ExcelMap SET ExcelBlock = @Surname Where CloumnName = 'Surname' ", conn);
+            //SqlCommand cmd = new SqlCommand("insert into ExcelMap ([Address], [Name], [Surname]) values(@Address,@Name,@Surname)",conn);
+            cmdAdrs.Parameters.AddWithValue("@adrs", (addressLoc));
+            cmdName.Parameters.AddWithValue("@Name", (nameLoc));
+            cmdSurname.Parameters.AddWithValue("@Surname", (surnameLoc));
+            cmdAdrs.ExecuteNonQuery();
+            cmdName.ExecuteNonQuery();
+            cmdSurname.ExecuteNonQuery();
+            conn.Close();
+            //MessageBox.Show("Mappings saved successfully");
+            notificationLabel.Text = String.Format("Mappings saved successfully");
+
+        }
+
+
+        private void addressBox(object sender, EventArgs e)
+        {
+            addressLoc = textBox1.Text;
+        }
+
+        private void nameBox(object sender, EventArgs e)
+        {
+            nameLoc = textBox2.Text;
+
+        }
+
+        private void surnameBox(object sender, EventArgs e)
+        {
+            surnameLoc = textBox3.Text;
+        }
+
+        private void SaveButton(object sender, EventArgs e)
+        {
+            conn.Open();
+            List<String> excelBlocks = new List<String>();
+            string query = "Select * from ExcelMap";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string output = reader.GetValue(1).ToString();
+                excelBlocks.Add(output.ToString());
+                //MessageBox.Show(output);               
+            }
+            foreach (String i in excelBlocks) { Console.WriteLine(i.ToString()); }
+
+            List<String> info = new List<String>();
+            string queryInfo = "Select * from Info";
+            SqlCommand cmdInfo = new SqlCommand(queryInfo, conn);
+            SqlDataReader readerInfo = cmdInfo.ExecuteReader();
+            while (readerInfo.Read())
+            {
+                info.Add(readerInfo.GetValue(0).ToString());
+                info.Add(readerInfo.GetValue(1).ToString());
+                info.Add(readerInfo.GetValue(2).ToString());
+
+            }
+            conn.Close();
+
+            //---Write to excel
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            app.Visible = true;
+            app.DisplayAlerts = false;
+            string fileName = "C:/Users/pc/source/repos/DataMap/DataMap/ExcelDnm.xlsx";
+            Microsoft.Office.Interop.Excel.Workbook workbook = app.Workbooks.Open(fileName);
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets[1];
+
+            worksheet.Range[excelBlocks[0]].Value = info[0].ToString();
+            worksheet.Range[excelBlocks[1]].Value = info[1].ToString();
+            worksheet.Range[excelBlocks[2]].Value = info[2].ToString();
+
+            workbook.SaveAs(Filename: @"C:\Users\pc\source\repos\DataMap\DataMap\ExcelDnm.xlsx", AccessMode: XlSaveAsAccessMode.xlNoChange);
+            //workbook.Close();
+            //app.Quit();
+
+            /*Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("Address", excelBlocks[0].ToString());
+            dictionary.Add("Name", excelBlocks[1].ToString());
+            dictionary.Add("Surname", excelBlocks[2].ToString());
+
+            string addressMap = dictionary["Address"].ToString();
+            string nameMap = dictionary["Name"].ToString();
+            string surnameMap = dictionary["Surname"].ToString();
+            Console.WriteLine(addressMap,surnameMap,nameMap);
+
+
+            foreach (KeyValuePair<string, string> kvp in dictionary)
+            {
+                Console.WriteLine(kvp.Key, kvp.Value.ToString());
+            } */
+        }
+
+        //--Make the window draggable
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void CloseButton(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void MinimizeButton(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+
+
+}
+
